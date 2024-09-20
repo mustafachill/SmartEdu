f1b7eeaf3765d5c90bf6189548ccc152090000e8
Mustafa Çil <mustafacilbusiness@gmail.com>
2024-09-20 18:43:20 +0300

    Course model created

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // IMPORTS
 const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
 
 // EXPRESS
 const app = express();
+
+//CONNECT TO DB
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfuly');
+  });
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -10,18 +24,8 @@ app.set('view engine', 'ejs');
 // MIDDLEWARES
 app.use(express.static('public'));
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+// ROUTE
+app.use('/', pageRoute);
 
 // PORT
 const port = 3000;
