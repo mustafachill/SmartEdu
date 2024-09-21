36663d8975461b74a823f684cd16510a3d2af1ae
Mustafa Çil <mustafacilbusiness@gmail.com>
2024-09-21 03:51:24 +0300

    detail page created, model and slug edited

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
 
 // EXPRESS
 const app = express();
@@ -9,10 +11,10 @@ const app = express();
 //CONNECT TO DB
 mongoose
   .connect('mongodb://localhost/smartedu-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
   })
   .then(() => {
     console.log('DB Connected Successfuly');
@@ -20,12 +22,15 @@ mongoose
 
 // TEMPLATE ENGINE
 app.set('view engine', 'ejs');
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // MIDDLEWARES
 app.use(express.static('public'));
 
 // ROUTE
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 // PORT
 const port = 3000;
