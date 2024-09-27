75e31712cf9001793e636967f61bd14b272543a0
Mustafa Çil <mustafacilbusiness@gmail.com>
2024-09-27 18:35:11 +0300

    course release mode added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const Course = require('../models/Course');
 const Category = require('../models/Category');
+const User = require('../models/User');
 
 exports.createCourse = async (req, res) => {
   try {
@@ -69,6 +70,23 @@ exports.enrollCourse = async (req, res) => {
     const user = await User.findById(req.session.userID);
     await user.courses.push({ _id: req.body.course_id });
     await user.save();
+
+    res.status(200).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error: error.message,
+    });
+  }
+};
+
+exports.releaseCourse = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({ _id: req.body.course_id });
+    await user.save();
+
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
